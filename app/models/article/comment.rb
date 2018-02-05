69a994b8d3d7a2f13b8c2ffecd175a9f91a4f036
Alexandr Borisov <ab@cifronomika.ru>
2018-02-05 10:32:31 +0300

    refactored like count validator

--- app/models/article/comment.rb
+++ app/models/article/comment.rb
@@ -2,7 +2,7 @@ class Article::Comment < ApplicationRecord
   belongs_to :article
   has_many :likes, dependent: :delete_all
 
-  def likeable?
+  def can_be_liked?
     likes_count_last_hour = likes.where('created_at >= ?', 1.hour.ago).count
     likes_count_last_hour < 5
   end
--- app/mutators/article/comment/like_mutator.rb
+++ app/mutators/article/comment/like_mutator.rb
@@ -1,7 +1,7 @@
 module Article::Comment::LikeMutator
   class << self
     def create!(comment, max_likes_count_per_hour: 5)
-      return unless comment.likeable?
+      return unless comment.can_be_liked?
       comment.likes.create! article: comment.article
     end
   end
